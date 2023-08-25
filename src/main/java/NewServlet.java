0a6e8000fe27a039a97dfb937f0da699970dc5aa
atmmtawa <134715890+atmmtawa@users.noreply.github.com>
2023-08-26 00:07:52 +0300

    Added space
    added spce to the code

--- src/main/java/NewServlet.java
+++ src/main/java/NewServlet.java
@@ -8,7 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class NewServlet
+
+ * Servlet  implementation class NewServlet
  */
 @WebServlet("/NewServlet")
 public class NewServlet extends HttpServlet {
