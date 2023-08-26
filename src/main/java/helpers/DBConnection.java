bbb95bec02952ba52f556a260d0ef23d6b5da174
Raxan7 <manyerere201@gmail.com>
2023-08-26 09:06:08 +0300

    Modified the DBConnection class

--- src/main/java/helpers/DBConnection.java
+++ src/main/java/helpers/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/yourdb_name";
-	private static final String USER = "username";
-	private static final String PASSWORD = "username";
+	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/FarmRentSystemDB";
+	private static final String USER = "saidi";
+	private static final String PASSWORD = "blender1";
 	
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
