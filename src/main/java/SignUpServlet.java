5fcfa5babb1aad2516af445978e8cdd8ac5bd410
Raxan7 <manyerere201@gmail.com>
2023-08-27 00:04:23 +0300

    MOdified Sign Up Servlet

--- src/main/java/SignUpServlet.java
+++ src/main/java/SignUpServlet.java
@@ -1,6 +1,6 @@
 
-
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
@@ -20,18 +20,26 @@ public class SignUpServlet extends HttpServlet {
 	/**
 	 * Sign Up Servlet provides the functionality to handle Sign Up into the system
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Connect to the actual form and send data into the database
+		
+		// I/O objects
+		PrintWriter out = response.getWriter();
 		
 		// // Capture data from the Sign Up form 
-		String first_name = request.getParameter("first_name");
-		String last_name = request.getParameter("last_name");
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
+//		String first_name = request.getParameter("first_name");
+//		String last_name = request.getParameter("last_name");
+//		String email = request.getParameter("email");
+//		String password = request.getParameter("password");
+		
+		String first_name = "Saidi";
+		String last_name = "Manyerere";
+		String email = "manyerere201@gmail.com";
+		String password = "blender1";
 		
 		// Connect to the database
 		try {
+			Class.forName("com.mysql.jdbc.Driver");
 			Connection conn = DBConnection.getConnection();
 			String sql = "INSERT INTO user (email, first_name, last_name, password) VALUES (?, ?, ?, ?)";
 			PreparedStatement statement = conn.prepareStatement(sql);
@@ -44,58 +52,13 @@ public class SignUpServlet extends HttpServlet {
 			// Close the resources
 			statement.close();
 			conn.close();
+			out.print("Successfully sent the data into the database");
 		} catch (Exception e) {
-			
+			e.printStackTrace();
+			out.print("An error occured");
 		}
 		
 		// Capture data from the Sign Up form and send it to the database
 		
 	}
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@WebServlet("/SignUpServlet")
-public class SignUpServlet extends HttpServlet {
- protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     String username = request.getParameter("username");
-     String password = request.getParameter("password");
-
-     try {
-         Connection conn = DBConnection.getConnection();
-         String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-         PreparedStatement statement = conn.prepareStatement(sql);
-         statement.setString(1, username);
-         statement.setString(2, password);
-         statement.executeUpdate();
-
-         // Close resources
-         statement.close();
-         conn.close();
-     } catch (Exception e) {
-         e.printStackTrace();
-     }
-
-     // Redirect or show a success message
- }
-}
-
