b21d06a5fc6dc163c58b26d043f0d68ada5877a1
Raxan7 <manyerere201@gmail.com>
2023-08-31 09:15:13 +0300

    Uploaded build folder

---
+++ build/classes/sql_scripts/database_script.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user (
+    email VARCHAR(100) PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    password VARCHAR(100)
+);
+
+CREATE TABLE farm (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    location VARCHAR(100),
+    description TEXT,
+    image LONGBLOB,
+    farm_size VARCHAR(5),
+    status VARCHAR(50) CHECK(status IN('RENT', 'PURCHASE')),
+    contact VARCHAR(100)
+)
