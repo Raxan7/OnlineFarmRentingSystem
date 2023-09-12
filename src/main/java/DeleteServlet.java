9fed85da6825db1a3d9a985bbc2ca7a27584e3d3
Raxan7 <manyerere201@gmail.com>
2023-09-12 11:08:04 +0300

    farm delete servelet

--- src/main/java/DeleteServlet.java
+++ src/main/java/DeleteServlet.java
@@ -9,13 +9,18 @@ import java.sql.*;
 
 public class DeleteServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         response.getWriter().append("Served at: ").append(request.getContextPath());
-	String f_id = request.getParameter("f_id");
+	String farm_id = request.getParameter("farm_id");
        // name = "Franciska";
-	System.out.println(f_id);
+//	System.out.println(farm_id);
         
         try{
             PrintWriter out = response.getWriter();
@@ -23,18 +28,26 @@ public class DeleteServlet extends HttpServlet {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_raxan7_db","freedb_saidi", "7*vtUS?fjyBFJg3");
             
+            PreparedStatement st = con.prepareStatement("SELECT sales_status FROM farm WHERE id='"+ farm_id +"'");
+            ResultSet rs = st.executeQuery();
             
-            PreparedStatement st = con.prepareStatement("delete from farm where id='"+ f_id + "'");
-            int i = st.executeUpdate();
-	    if (i==0){
-		out.println("Row has been deleted");
-	    }
-	    else{
-		out.println("No rows has been deleted");
-	    }
-            
-            response.sendRedirect("MyFarm.jsp");
+            while(rs.next()) {
+//            	System.out.println(rs.getString(1));
+            	if(rs.getString(1)=="AVAILABLE") {
+                  PreparedStatement stmt = con.prepareStatement("DELETE FROM farm WHERE id='"+ farm_id + "'");
+                  stmt.executeUpdate();	
+            	}
+            	else {
+//            		System.out.println("You can't delete the farm is already sold");
+            		response.sendRedirect("soldFarmError.jsp?farm_id="+ farm_id);
+            	}
+            }
+//            PreparedStatement st = con.prepareStatement("DELETE FROM farm WHERE id='"+ farm_id + "'");
+//            st.executeUpdate();
+	  
             con.close();
+            response.sendRedirect("MyFarm.jsp");
+            
 
     }
         catch(Exception e){
